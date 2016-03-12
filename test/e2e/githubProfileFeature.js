5bd5ebce9e3310ca9b9cd6c6b5eb540694848bf0
Vicky Gray <vickygray26@hotmail.com>
2016-03-12 21:57:21 +0000

    added basic css

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -11,12 +11,6 @@ describe('GitHub profile finder', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
 
-  // it('finds profiles', function() {
-  //   searchBox.sendKeys('spike01');
-  //   searchButton.click();
-  //   expect(element(by.binding('user.login')).getText()).toEqual('spike01');
-  // });
-
   it('finds profiles', function() {
 
     searchBox.sendKeys('spike');
