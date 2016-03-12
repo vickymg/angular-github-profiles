92bbea38218c417794aad84b52df3f96dca42ff3
Vicky Gray <vickygray26@hotmail.com>
2016-03-12 18:37:33 +0000

    completed searchFactory

--- test/searchfactory.spec.js
+++ test/searchfactory.spec.js
@@ -2,14 +2,43 @@ describe('factory: Search', function() {
 
   var search;
 
+  var items = [
+    {
+      "login": "tansaku",
+      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+      "html_url": "https://github.com/tansaku"
+    },
+    {
+      "login": "stephenlloyd",
+      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+      "html_url": "https://github.com/stephenlloyd"
+    }
+  ];
+
   beforeEach(module('GitUserSearch'));
 
   beforeEach(inject(function(Search) {
     search = Search;
   }));
 
+  beforeEach(inject(function($httpBackend) {
+    httpBackend = $httpBackend;
+    httpBackend
+      .when("GET", "https://api.github.com/search/users?q=hello")
+      .respond(
+        { items: items }
+      );
+  }));
+
   it('responds to query', function() {
     expect(search.query).toBeDefined();
   });
 
+  it('returns search results', function() {
+    search.query('hello')
+      .then(function(response) {
+        expect(response.data.items).toEqual(items);
+      });
+    httpBackend.flush();
+  });
 });
