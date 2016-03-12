90eb15a7e7472f0dd2e75f5b875c32be6e9df0a2
Vicky Gray <vickygray26@hotmail.com>
2016-03-12 20:40:32 +0000

    set up protractor

---
+++ test/e2e/githubProfileFeature.js
@@ -0,0 +1,20 @@
+describe('GitHub profile finder', function() {
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'));
+  var searchButton = element(by.className('btn'));
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github user search');
+  });
+
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+  });
+
+});
